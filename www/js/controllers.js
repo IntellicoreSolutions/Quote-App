db77e0e86ea059dec004d0803a882daf9e66bcdf
Ross Thomson <ross.thomson@intellicore.co.uk>
2016-05-18 15:28:49 +0100

    Feedback Input setup for API

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,10 @@ angular.module('starter.controllers', [])
       $window.location.href = '#/feedback';
     }
 
+    /////////////////////////////////////
+    //Lead page submit without feedback /
+    /////////////////////////////////////
+
     $scope.leadButtonFinish = function(){
       $ionicLoading.show({
         template: 'Loading...'
@@ -20,7 +24,7 @@ angular.module('starter.controllers', [])
         company: $scope.company,
         industry: $scope.industry,
         website: $scope.website,
-        street: $scope.address,
+        street: $scope.street,
         postcode: $scope.postcode,
         city: $scope.city,
         employees: $scope.staff,
@@ -28,31 +32,11 @@ angular.module('starter.controllers', [])
         notes: $scope.notes,
       }
 
-
-      var feedback = {
-        topic: $scope.topic,
-        firstName: $scope.firstName,
-        lastName: $scope.surname,
-        jobTitle: $scope.job,
-        telephone: $scope.telephone,
-        email: $scope.email,
-        leadSource: $scope.lead,
-        company: $scope.company,
-        industry: $scope.industry,
-        website: $scope.website,
-        street: $scope.address,
-        postcode: $scope.postcode,
-        city: $scope.city,
-        employees: $scope.staff,
-        operatives: $scope.operatives,
-        notes: $scope.notes,
-      }
-
-       console.log(form);
+      console.log(form);
 
       $http({
 
-        url: 'http://192.168.1.12:5562/crmapi/createlead',
+        //url: 'http://192.168.1.12:5562/crmapi/createlead',
         method: 'POST',
         data: form,
         headers: {'Content-Type': 'application/json'}
@@ -79,12 +63,69 @@ angular.module('starter.controllers', [])
           template: 'Please fill in the topic'
         });
       }
-      //alert($scope.firstName);
-      //alert($scope.surname);
-      //alert($scope.job);
-      //alert($scope.telephone);
-      //alert($scope.email);
     }
+
+
+    /////////////////////////////////////
+    //////// Feedback Page Submit ///////
+    /////////////////////////////////////
+
+    $scope.finish = function(){
+
+      var feedback = {
+        new_doyouhaveasystemalready: $scope.system,
+        new_whatisthecurrentsystemcalled: $scope.systemName,
+        new_costofcurrentsystem: $scope.systemCost,
+        new_howdoyoucurrentlypayforyoursystem: $scope.systemPayment,
+        new_currentsystemcomments: $scope.systemNotes,
+
+        new_followupdemo: $scope.demo,
+        new_date1: $scope.date1,
+        new_date2: $scope.date2,
+
+        new_doyouliketheui: $scope.ui,
+        new_uicomments: $scope.uiComments,
+
+        new_didtheprocessmakesense: $scope.sense,
+        new_processcomments: $scope.senseComments,
+
+        new_anypartoftheprocessyouwouldntuse: $scope.process,
+        new_notusedcomments: $scope.processComments,
+
+        new_anyfeaturesyouwouldlikeadded: $scope.features,
+        new_addedfeaturecomments: $scope.featuresComments,
+
+        new_tabletsorphones: $scope.mobile,
+      }
+
+      console.log(feedback);
+
+      $http({
+
+        //url: 'http://192.168.1.12:5562/crmapi/createlead',
+        method: 'POST',
+        data: feedback,
+        headers: {'Content-Type': 'application/json'}
+
+
+      }).success(function (data, status, header, config){
+
+        $ionicLoading.hide();
+
+        console.log(data, status);
+
+      }).error(function (data, status, header, config) {
+
+        $ionicLoading.hide();
+
+        console.log(data, status);
+
+      });
+
+    }
+
+
+
 
     $scope.startAgainButton = function() {
       $window.location.href = '#/lead';
